8b5bd945de0af2c21413e647772de168e4445ba9
hwjk16322@gmail.com <hwjk16322@gmail.com>
2024-01-10 13:02:22 +0900

    test

--- src/main/java/com/example/sbbtest/MainController.java
+++ src/main/java/com/example/sbbtest/MainController.java
@@ -1,13 +1,28 @@
 package com.example.sbbtest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
+    int vote = 1;
+
     @GetMapping("/")
-    public String root(){
-        return "redirect:/article/list";
+    public String root(Model model) {
+
+        model.addAttribute("vote",vote);
+        return "vote";
+    }
+
+    @GetMapping("/vote")
+    @ResponseBody
+    public String Vote() {
+        vote++;
+        Integer count = this.vote;
+        return count.toString();
     }
 }
+
--- src/main/java/com/example/sbbtest/QuestionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.sbbtest;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepository extends JpaRepository<Question,Integer> {
-}
